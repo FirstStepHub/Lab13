2a21c21caecb50ea6e1a1c7b26de635bf5456c5a
FirstStepHub <first_yo1@hotmail.com>
2026-01-09 15:50:23 +0700

    lab13_1_complete.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,13 +3,13 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double[]);
+void stat(const double[], int, double[]);
 
-int main(){
-    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
-    int N = sizeof(A)/sizeof(A[0]);
+int main() {
+    double A[] = {1.2, 3.5, 6.9, 7.8, 12.5, 0.5};
+    int N = sizeof(A) / sizeof(A[0]);
     double B[6];
-    stat(A,N,B);
+    stat(A, N, B);
     cout << fixed << setprecision(2);
     cout << "Arithmetic Mean = " << B[0] << endl;
     cout << "Standard Deviation = " << B[1] << endl;
@@ -18,4 +18,33 @@ int main(){
     cout << "Max = " << B[4] << endl;
     cout << "Min = " << B[5];
     return 0;
+}
+
+void stat(const double A[], int N, double B[]) {
+    double sum = 0 ;
+    double sum_sq_diff = 0 ;
+    double product = 1 ;
+    double sum_recip = 0 ;
+    double max = A[0] ;
+    double min = A[0] ;
+
+    for (int i = 0; i < N; i++) {
+        sum += A[i] ;
+        product *= A[i] ;
+        sum_recip += (1.0 / A[i]) ;
+
+        if (A[i] > max) max = A[i] ;
+        if (A[i] < min) min = A[i] ;
+    }
+
+    B[0] = sum / N;
+    B[2] = pow(product, 1.0 / N) ;
+    B[3] = N / sum_recip ;
+    B[4] = max ;
+    B[5] = min ;
+
+    for (int i = 0; i < N; i++) {
+        sum_sq_diff += pow(A[i] - B[0], 2) ;
+    }
+    B[1] = sqrt(sum_sq_diff / N) ;
 }
